b55510a4d0d262aedd3748e4b465cacc4d69fdc2
Jovica Zlatanovic <jovica.zlatanovic@htecgroup.com>
2021-04-05 23:31:56 +0200

    TF test fmt

--- apps/terraform/tmpl/test/example_test.go
+++ apps/terraform/tmpl/test/example_test.go
@@ -1,26 +1,26 @@
 package test
 
 import (
-  "testing"
+	"testing"
 
-  "github.com/gruntwork-io/terratest/modules/terraform"
-  "github.com/stretchr/testify/assert"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestTerraformExample(t *testing.T) {
-  // Arrange
-  terraformOptions := &terraform.Options{
-    TerraformDir: "../example/.",
-  }
-  defer terraform.Destroy(t, terraformOptions)
+	// Arrange
+	terraformOptions := &terraform.Options{
+		TerraformDir: "../example/.",
+	}
+	defer terraform.Destroy(t, terraformOptions)
 
-  // Act
-  terraform.InitAndApply(t, terraformOptions)
+	// Act
+	terraform.InitAndApply(t, terraformOptions)
 
-  // Assert
-  assert := assert.New(t)
+	// Assert
+	assert := assert.New(t)
 
-  outputValue := terraform.Output(t, terraformOptions, "output_name")
-  assert.NotNil(outputValue)
-  assert.Equal("output_value", outputValue)
+	outputValue := terraform.Output(t, terraformOptions, "output_name")
+	assert.NotNil(outputValue)
+	assert.Equal("output_value", outputValue)
 }
